b9cf7533087c774f6fd319ea16a4caacd7d415f3
Takenori Nakagawa <ww24gm@gmail.com>
2015-12-10 16:27:39 +0900

    default download file type is JPEG

--- storage/storage.go
+++ storage/storage.go
@@ -103,11 +103,11 @@ func (storage *Storage) Fetch(id string, extension string) (buff bytes.Buffer, m
 			NumColors: 256,
 		})
 		mimeType = "image/gif"
-	case "":
-		fallthrough
 	case ".png":
 		err = png.Encode(&buff, img)
 		mimeType = "image/png"
+	case "":
+		fallthrough
 	case ".jpg":
 		err = jpeg.Encode(&buff, img, &jpeg.Options{
 			Quality: 100,
